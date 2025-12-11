f33145a05612c8c05d123bc32c076dc549b1c468
zahraekaambiya <zahraekaambiya@gmail.com>
2025-12-11 15:41:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 st.title(":red[My project LPK 2025"])
 st.write(":orange[NTP], *IS COOL!* :sunglasses:")
+
+number = st.number_input("Insert a number",min_value=0, max_value=0)
+if number%2==1:
+  st.write("Bilangan",number,"termasuk bilangan ganjil")
+else:
+  st.write("Bilangan",number,"termasuk bilangan genap")
